fae4fc77e81d995f28d931c5b447acb1dc4eefa1
bharat882 <bharatsinghsaini00@gmail.com>
2024-03-25 22:53:03 -0400

    Products API added #1

---
+++ BackEnd/products.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/config.php';
+
+class API{
+    function viewProducts(){
+        $db = new Connect;
+        $products = array();
+        $data = $db->prepare('SELECT * FROM products');
+        $data->execute();
+        $i=0;
+        while($OutputData=$data->fetch(PDO::FETCH_ASSOC))
+        {
+            $products[$i]=array(
+                'image'=>$OutputData['image'],
+                'Title' =>$OutputData['Title']
+            );
+            $i++;
+        }
+        return json_encode($products);
+    }
+}
+
+$API = new API();
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+echo $API->viewProducts();
